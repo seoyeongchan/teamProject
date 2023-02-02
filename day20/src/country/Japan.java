c4f6d061cc266f57dd839106731ea1669df98a81
seoyeongchan <enfkssh0409@gmail.com>
2023-02-02 18:12:48 +0900

    커밋 테스트

---
+++ day20/src/country/Japan.java
@@ -0,0 +1,13 @@
+package country;
+
+public class Japan {
+	
+	public String name = "일본";
+	public int peopleSu = 67;
+	
+	public void sayHello() {
+		System.out.println("안녕");
+		System.out.println("나도안녕하세요");
+	}
+
+}
